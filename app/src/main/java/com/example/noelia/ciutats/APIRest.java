d481f11c3ff9708111a3b84fbc6c429a12eebbe0
Lestrigones <40467401+Lestrigones@users.noreply.github.com>
2018-12-20 11:07:01 +0100

    Examen

---
+++ app/src/main/java/com/example/noelia/ciutats/APIRest.java
@@ -0,0 +1,35 @@
+package com.example.noelia.ciutats;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface APIRest {
+
+
+
+
+    @GET("pag-ini/{ini}/pag-fi/{fi}")
+    Call<Cities> getCities(@Path("ini") int ini, @Path("fi") int fi);
+
+
+
+    static APIRest createDibaAPI() {
+        Gson gson = new GsonBuilder()
+                .setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+                .create();
+
+        String BASE_URL = "https://do.diba.cat/api/dataset/municipis/format/json/";
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        return retrofit.create(APIRest.class);
+    }
+}
